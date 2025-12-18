814ddbccc8015056cf04a742772e40fd3213ca52
Swagat210 <swagatkumar210@gmail.com>
2025-12-18 18:13:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ ADMIN_ID = int(os.getenv("7990200132"))
 CHANNEL_ID = int(os.getenv("1003692549136"))
 
 UPI_ID = os.getenv("7205105264@ibl")
-PLAN_PRICE = int(os.getenv("PLAN_PRICE", 99))
+PLAN_PRICE = int(os.getenv("PLAN_PRICE", 99)) 
 PLAN_DAYS = int(os.getenv("PLAN_DAYS", 30))
