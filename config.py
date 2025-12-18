6f0f865794cdea55e9bd77b60b308e36c59a4948
Swagat210 <swagatkumar210@gmail.com>
2025-12-18 17:56:18 +0530

    Update config.py to use environment variables
    Refactor config to use environment variables for sensitive data.

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-BOT_TOKEN = "8091152431:AAH9tplltohYVFS4OlbXJTontRoSz_te35Q"
-ADMIN_ID = 7990200132
-UPI_ID = "7205105264@ibl"
-CHANNEL_ID = -1003692549136
-PLAN_PRICE = 99
-PLAN_DAYS = 30
+import os
+
+BOT_TOKEN = os.getenv("8091152431:AAH9tplltohYVFS4OlbXJTontRoSz_te35Q")
+ADMIN_ID = int(os.getenv("7990200132"))
+CHANNEL_ID = int(os.getenv("1003692549136"))
+
+UPI_ID = os.getenv("7205105264@ibl")
+PLAN_PRICE = int(os.getenv("PLAN_PRICE", 99))
+PLAN_DAYS = int(os.getenv("PLAN_DAYS", 30))
